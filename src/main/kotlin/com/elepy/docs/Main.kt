ef28b3285e8866060f60b1bdccccd6e440d75e11
Ryan Susana <ryansusana@live.com>
2019-01-13 22:31:51 +0100

    Removed indecisive comments.

--- src/main/kotlin/com/elepy/docs/Main.kt
+++ src/main/kotlin/com/elepy/docs/Main.kt
@@ -133,7 +133,5 @@ fun updateGithub(name: String, content: String) {
 }
 
 fun GitHub.elepy(): GHRepository {
-
-    // STILL DECIDING IF I WANT IT TO AUTO-UPDATE ELEPY REPO
     return this.getRepository(if (System.getenv("testing") == null) "RyanSusana/elepy" else "RyanSusana/elepy-docs")
 }
